6f13ec6a382d4fce43e822b6add5620f6522bf5b
Beomsu Shin <s.bumsu@gmail.com>
2025-09-02 23:39:18 +0900

    FEAT: initiate admin app

---
+++ apps/admin/src/App.tsx
@@ -0,0 +1,17 @@
+import {useState} from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>TODO</p>
+        <p>TABLE (GRID)</p>
+        <p>ReBAC</p>
+        <p>DESIGN SYSTEM</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
